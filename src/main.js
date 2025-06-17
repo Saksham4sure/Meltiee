4de0e820441d41dc34b8d26dd31ecd9f4854bddd
Saksham Shrestha <saksham@Sakshams-MacBook-Pro.local>
2025-06-17 11:00:32 +0545

    fixed

--- src/main.js
+++ src/main.js
@@ -20,21 +20,21 @@ const lenis = new Lenis({
 
 
 let images = [
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpg",
-    "images/img4.jpg",
-    "images/img5.jpg",
-    "images/img6.jpg",
-    "images/img7.jpg",
-    "images/img8.jpg",
-    "images/img9.jpg",
-    "images/img10.jpg",
-    "images/img11.jpg",
-    "images/img12.jpg",
-    "images/img13.jpg",
-    "images/img14.jpg",
-    "images/img15.jpg"
+    "/images/img1.jpg",
+    "/images/img2.jpg",
+    "/images/img3.jpg",
+    "/images/img4.jpg",
+    "/images/img5.jpg",
+    "/images/img6.jpg",
+    "/images/img7.jpg",
+    "/images/img8.jpg",
+    "/images/img9.jpg",
+    "/images/img10.jpg",
+    "/images/img11.jpg",
+    "/images/img12.jpg",
+    "/images/img13.jpg",
+    "/images/img14.jpg",
+    "/images/img15.jpg"
 ]
 
 let home = document.querySelector(".home");
